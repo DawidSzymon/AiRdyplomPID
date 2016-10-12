2d26ce16a24c4b132d11423099b882143dd083ee
Dawid Cieszyński <Dawid Cieszyski>
2016-10-13 00:58:12 +0200

    dodatkowe opcje algorytmu "migracje, frakcje pareto"

--- algorytm.m
+++ algorytm.m
@@ -2,7 +2,7 @@ clear
 clc
 %%OGRANICZENIA
 A = [1 0 0; 0 1 0; 0 0 1];
-b = [200;300;10];
+b = [0;0;0];
 tic
 model = 'PIDsimulink';
 load_system(model);
@@ -11,6 +11,10 @@ load_system(model);
 % DISPLAY - œledzenie pracy algorytmu genetycznego
 % POPULATIONSIZE - liczba populacji 
 % CROSSOVERFRACTION - metoda krzy¿owa (domyœlnie ustawiano jest na 80%)
-options = gaoptimset('Generations',50,'Display','iter','PopulationSize',100,'CrossoverFraction',0.5);
-output = ga(@funkcja,3,[],[],[],[],[0;0;0],[200;200;200],[],options);
+options = gaoptimset('Generations',2000,'Display','iter','PopulationSize',600,'CrossoverFraction',0.8);
+output = ga(@funkcja,3,[],[],[],[],[],[],[],options);
 toc
+
+%,'ParetoFraction',0.35 ,'MigrationDirection','both'
+%output = ga(@funkcja,3,[],[],[],[],[0;0;0],[200;200;200],[],options);
+%b = [200;300;10];
